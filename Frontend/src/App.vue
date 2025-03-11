5a134820d3f7c44a9b07f47f93bfa17de3193ae2
polyakale <polyakalex52@gmail.com>
2025-03-11 12:16:50 +0100

    commit

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -40,8 +40,9 @@ export default {
   left: 0;
   width: 20vw;
   height: 100%; /* Ensures it spans the entire height of the page */
-  background: url('@/assets/filmReel2.png') repeat-y;
+  background: url('@/assets/filmReel3.png') repeat-y;
   background-size: 60%; /* Adjust vertical scaling of the image */
+  /* opacity: 0.6; */
   z-index: -1;
 }
 
@@ -52,8 +53,9 @@ export default {
   right: 0;
   width: 23.4vh;
   height: 100%; /* Ensures it spans the entire height of the page */
-  background: url('@/assets/filmReel2.png') repeat-y;
+  background: url('@/assets/filmReel4.png') repeat-y;
   background-size: 100%; /* Use the natural size for tiling vertically */
+  /* opacity: 0.6; */
   z-index: -1;
 }
 
