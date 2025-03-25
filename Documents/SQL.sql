4f444524a579eb8a22e8b9f7ed09a0a73d9bf646
HajduIstvan02 <hajduistvan@ugmszi.hu>
2025-03-25 12:57:43 +0100

    Rendrakás pipa

---
+++ Documents/SQL.sql
@@ -0,0 +1,22 @@
+﻿SELECT * FROM films fi;
+
+SELECT * FROM favourites fa;
+
+
+
+SELECT fi.id, 
+       fi.title, 
+       fi.production, 
+       fi.length, 
+       fi.presentation, 
+       fi.imdbLink, 
+       ROUND(AVG(fa.evaluation),1) AS evaluation 
+FROM films fi
+LEFT JOIN favourites fa ON fi.id = fa.filmId
+GROUP BY fi.id,
+        fi.title, 
+       fi.production, 
+       fi.length, 
+       fi.presentation, 
+       fi.imdbLink
+ORDER BY evaluation DESC;
