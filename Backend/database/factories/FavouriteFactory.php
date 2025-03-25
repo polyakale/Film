af57cabc0166f8fc98cdb5a15f30ac88f5d0ab3b
polyakale <polyakalex52@gmail.com>
2025-03-25 14:01:06 +0100

    proper favourite factory, works

--- Backend/database/factories/FavouriteFactory.php
+++ Backend/database/factories/FavouriteFactory.php
@@ -4,28 +4,46 @@ namespace Database\Factories;
 
 use App\Models\Film;
 use App\Models\User;
+use App\Models\Favourite;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\DB;
 use Faker\Factory as Faker;
 
 class FavouriteFactory extends Factory
 {
     public function definition(): array
-    {
-        $faker = Faker::create('hu_HU');
-
-        do {
-            $userId = User::inRandomOrder()->first()->id;
-        } while ($userId == 1);
-
-        $filmId = Film::inRandomOrder()->first()->id;
-
-        // Generate ratings in 0.5 increments from 0.5 to 5
-        $evaluation = $faker->numberBetween(1, 10) / 2;
-
-        return [
-            'userId' => $userId,
-            'filmId' => $filmId,
-            'evaluation' => $evaluation
-        ];
+{
+    $faker = Faker::create('hu_HU');
+    
+    static $possibleCombinations;
+    
+    if (!isset($possibleCombinations)) {
+        $userIds = User::where('id', '!=', 1)->pluck('id');
+        $filmIds = Film::pluck('id');
+        
+        $possibleCombinations = collect($userIds)
+            ->crossJoin($filmIds)
+            ->shuffle()
+            ->all();
+            
+        $existingPairs = Favourite::select(['userId', 'filmId'])
+                                ->get()
+                                ->map(fn($item) => [$item->userId, $item->filmId])
+                                ->all();
+                                
+        $possibleCombinations = array_diff($possibleCombinations, $existingPairs);
     }
+    
+    if (empty($possibleCombinations)) {
+        throw new \Exception("No more unique user-film combinations available");
+    }
+    
+    $combination = array_pop($possibleCombinations);
+    
+    return [
+        'userId' => $combination[0],
+        'filmId' => $combination[1],
+        'evaluation' => $faker->numberBetween(1, 10) / 2
+    ];
+}
 }
